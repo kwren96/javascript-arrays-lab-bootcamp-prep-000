1b7f45cc351f849ab4976005b920cfb8e692cb1d
Katherine Wren <kwren@outlook.com>
2019-03-26 14:37:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ function destructivelyRemoveLastKitten() {kittens.pop(); return kittens}
 function destructivelyRemoveFirstKitten() {kittens.shift(); return kittens}
 function appendKitten(name) {return [...kittens, name]}
 function prependKitten(name) {return [name, ...kittens]}
-function removeLastKitten() {return kittens.slice(0, kitten.length-1)}
+function removeLastKitten() {return kittens.slice(0, kittens.length-1)}
